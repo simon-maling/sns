743ab702b77699ed3bec99a829c09f326af7963b
Simon M <siz111@gmail.com>
2019-02-01 11:14:42 +0100

    fix sns sqs integration to set content type as app json and to directory send body instead of wrapper

--- src/main/scala/me/snov/sns/actor/ProducerActor.scala
+++ src/main/scala/me/snov/sns/actor/ProducerActor.scala
@@ -16,14 +16,16 @@ class ProducerActor(endpoint: String, subscriptionArn: String, topicArn: String)
 
   override def transformOutgoingMessage(msg: Any) = msg match {
     case snsMsg: Message => {
-      new CamelMessage(snsMsg.toJson.toString, Map(
-
+      val message = snsMsg.bodies("default")
+      
+      new CamelMessage(message, Map(
         CamelMessage.MessageExchangeId -> snsMsg.uuid,
         Exchange.HTTP_METHOD -> HttpMethods.POST,
         "x-amz-sns-message-type" -> "Notification",
         "x-amz-sns-message-id" -> snsMsg.uuid,
         "x-amz-sns-subscription-arn" -> subscriptionArn,
-        "x-amz-sns-topic-arn" -> topicArn
+        "x-amz-sns-topic-arn" -> topicArn,
+        "contentType" -> "application/json"
       ))
     }
   }
